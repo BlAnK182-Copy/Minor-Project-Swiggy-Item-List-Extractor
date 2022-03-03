9b34af52cb9ad2e12743911c8f1b92151d51b1fc
Abhinav <abhinav.30.kr@gmail.com>
2022-03-03 21:57:53 +0530

    Updated GUI
    Changed the GUI code - frames and organisation

--- Assets/GUI.py
+++ Assets/GUI.py
@@ -10,30 +10,40 @@ class GUI:
 
         self.window=Tk()
 
-        self.window.geometry('1000x500')
+        self.windowHeight = c.WINDOW_HEIGHT
+        self.windowWidth = c.WINDOW_WIDTH
+        # self.window.resizable(width=TRUE, height=TRUE)
+        self.window.geometry(f'{self.windowWidth}x{self.windowHeight}')
+        # self.window.geometry("")
+        # self.window.wm_attributes('-transparentcolor','black')
 
+        #bgImage
         self.bgImage=PhotoImage(file=c.BG_IMAGE_PATH)
         self.bgImageLabel=Label(self.window,image=self.bgImage)
-        self.bgImageLabel.place(x=0,y=0)
+        self.bgImageLabel.grid(row=0,column=0, sticky="NSEW")
 
-        self.canvas=Canvas(self.window,height='350',width='700',bg='orange')
-        self.canvas.place(x=325,y=175)
+        #custom frame
+        self.frame=Frame(self.window,height=f'{self.windowHeight * (1/2)}',width=f'{self.windowWidth * (1/2)}',bg='orange')
+        self.frame.grid(row=0,column=0, padx=(self.windowWidth * (1/4)), pady=(self.windowHeight * (1/4)))
 
-        self.greetingWidget=Label(self.window,text="Welcome to PESU Eateries!!",font="Calibri 24 bold",bg=None)
-        self.greetingWidget.place(x=475,y=190)
+        #greeting 
+        self.greetingWidget=Label(self.frame,text="Welcome to PESU Eateries!!",font="Consolas 24 bold italic", bg = None)
+        self.greetingWidget.grid(row = 0, column = 0, padx = c.X_PADDING, pady = c.Y_PADDING)
 
-        self.titleWidget=Label(self.window,text='Enter the Swiggy-URL of the restaurant:',font='Courier 8')
-        self.titleWidget.place(x=330,y=250)
-
-        self.UrlEntryWidget=Entry(self.window,borderwidth='0')
-        self.UrlEntryWidget.place(x=625,y=250)
+        #title
+        self.titleWidget=Label(self.frame,text='Enter the Swiggy-URL of the restaurant:',font='Courier 8')
+        self.titleWidget.grid(row = 1, column = 0, padx = c.X_PADDING)
+        
+        #url entry
+        self.UrlEntryWidget=Entry(self.frame,borderwidth='0')
+        self.UrlEntryWidget.grid(row = 2, column= 0, padx = c.X_PADDING)
         
         self.restaurant_name=self.UrlEntryWidget.get()
 
         self.submitButtonImage=PhotoImage(file=c.BUTTON_IMAGE_PATH)
-        self.submitButton=Button(self.window,image=self.submitButtonImage,command=self.submitButtonClick,border=0)
+        self.submitButton=Button(self.frame,image=self.submitButtonImage,command=self.submitButtonClick,border=0)
         self.UrlEntryWidget.bind('<Return>',self.enterButton)
-        self.submitButton.place(x=510,y=280)
+        self.submitButton.grid(row = 3, column=0, padx=c.X_PADDING, pady=c.Y_PADDING)
 
         self.window.mainloop()
             
@@ -60,7 +70,7 @@ class GUI:
             #temporaily calling a tksheet
             self.table = Table_Generator()
             root = Tk()
-            table1= self.table.get_all_contents(root,c.FILENAME, display_category=True)
+            table1= self.table.get_all_contents(root,c.FILENAME, display_categorcolumn=True)
             table1.pack()
 
 
